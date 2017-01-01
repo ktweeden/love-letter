c247f3ca29b9fbf40da5e2d18fc7e42a0fa7a6a5
Kate Preston <ktweeden@googlemail.com>
2017-01-01 18:39:59 +0000

    created player hands with remove and add functions

--- game/deck.js
+++ game/deck.js
@@ -10,9 +10,6 @@ class Deck {
       }
     })
   }
-
-
-
 }
 
 module.exports = Deck
---
+++ game/player.js
@@ -0,0 +1,25 @@
+
+class Player {
+  constructor(){
+    this._hand = []
+  }
+
+  get hand() {
+    return this._hand
+  }
+
+  removeCardFromHand(cardToRemove) {
+    const newHand = []
+    this.hand.forEach((card) => {
+      if (card.name !== cardToRemove.name) {
+        newHand.push(cardToRemove)
+      }
+    })
+    
+    this._hand = newHand
+  }
+
+  addCardToHand(card) {
+    this._hand.push(card)
+  }
+}
